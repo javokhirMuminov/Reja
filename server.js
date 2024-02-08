0781b1ae1236ebd283db886afb08a5c8da26c5f2
javokhirMuminov <javokhirmuminovb@gmail.com>
2024-02-08 14:51:17 +0900

    Add  Portfolio

--- server.js
+++ server.js
@@ -2,7 +2,17 @@ console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+// const res = require("express/lib/response");
 
+let user;
+fs.readFile("database/user.json", "utf8", (err,data) => {
+  if(err) {
+    console.log("Error", err);
+  }else {
+    user = JSON.parse(data);
+  }
+})
 
 
 // 1 - chi bosqich kirish code
@@ -21,10 +31,18 @@ app.set("view engine", "ejs");
 
 
 //4 Routing code
+// app.post("/create-item", (req, res) => {
+//   console.log(req.body);
+//   res.json({test:"success"});
+// })
+
 app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  res.json({test:"success"});
-})
+  //TODO: code with db here
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", {user:user} );
+});
 
 app.get("/", function (req, res) {
   res.render("harid");

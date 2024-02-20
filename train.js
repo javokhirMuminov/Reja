46a6d5af51b0de1b6317eea425bd8d2b5ac05796
javokhirMuminov <javokhirmuminovb@gmail.com>
2024-02-20 16:38:06 +0900

    28 ~32 lesson hommevork

--- train.js
+++ train.js
@@ -1,6 +1,50 @@
 
-//Masala 2
-function sonlar(str) {
-  let regex = /[^0-9]/g;
-  return str.replace(regex, "").length;
+//Masala 3
+// class Shop {
+//   constructor(non, cola, lagmon) {
+//     this.non = non;
+//     this.cola = cola;
+//     this.lagmon = lagmon;
+//   }
+
+//   qabul(type, num) {
+//     if (type === "non") {
+//       this.non = this.non + num;
+//     } else if (type === "cola") {
+//       this.cola = this.cola + num;
+//     } else {
+//       this.lagmon = this.lagmon + num;
+//     }
+//   }
+//   sotish(type, num) {
+//     if (type === "non") {
+//       this.non = this.non - num;
+//     } else if (type === "cola") {
+//       this.cola = this.cola - num;
+//     } else {
+//       this.lagmon = this.lagmon - num;
+//     }
+//   }
+//   qoldiq() {
+//     return `hozir ${new Date().getHours()} : ${new Date().getMinutes()} da ${
+//       this.non
+//     } ta non, ${this.lagmon} ta lagmon va ${this.cola} ta cola mavjud!`;
+//   }
+// }
+
+
+
+
+
+//Masala 4
+
+// Task - E
+function solution(str) {
+  let answer = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    answer += str[i];
+  }
+  return answer;
 }
+
+console.log(solution("hello"));

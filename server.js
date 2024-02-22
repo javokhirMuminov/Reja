87d0a3e2f7e3e7f679405fc3b840b0e1bd8c14bc
javokhirMuminov <javokhirmuminovb@gmail.com>
2024-02-22 16:08:42 +0900

    123

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongodb.connect(
       module.exports = client;
       const app = require("./app");
       const server = http.createServer(app);
-      let PORT = 3000;
+      let PORT = 7004;
       server.listen(PORT, function () {
         console.log(
           `This server is running seccessfully on port: ${PORT}, http://localhost:${PORT}`
